1b466a5cdf8a434caabedfa3ee0d86d25eda9f16
hieuma9029 <saiden9029@gmail.com>
2026-07-15 15:51:08 +0700

    done bai 11

--- src/create_new_product/Main.java
+++ src/create_new_product/Main.java
@@ -1,8 +1,88 @@
-package createnewproduct;
+package create_new_product;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static List<Product> createListProduct() {
 
+        List<Product> products = new ArrayList<>(
+                Arrays.asList(
+                        new Product(1, "Laptop", 1,
+                                LocalDate.of(2026,1,1),
+                                280,
+                                false),
+                        new Product(2, "Iphone", 2,
+                                LocalDate.of(2025,11,16),
+                                240,
+                                true),
+                        new Product(3, "Samsung", 3,
+                                LocalDate.of(2025,12,9),
+                                203,
+                                false),
+                        new Product(4, "PC", 4,
+                                LocalDate.of(2025,10,10),
+                                201,
+                                true),
+                        new Product(5, "Mouse", 5,
+                                LocalDate.of(2025,8,19),
+                                200,
+                                false),
+                        new Product(6, "Ipad", 6,
+                                LocalDate.of(2025,12,27),
+                                202,
+                                false),
+                        new Product(7, "Macbook", 7,
+                                LocalDate.of(2026,1,21),
+                                92,
+                                true),
+                        new Product(8, "Headphone", 8,
+                                LocalDate.of(2026,1,16),
+                                29,
+                                false),
+                        new Product(9, "Screen", 9,
+                                LocalDate.of(2026,1,1),
+                                29,
+                                false),
+                        new Product(10, "Chair", 10,
+                                LocalDate.of(2025,12,1),
+                                220,
+                                true)
+    )
+        );
+
+        return products;
+    }
+
+    //k dùng steam
+//    public static String filterProductById(List<Product> listProduct, int idProduct) {
+//
+//        for (Product product : listProduct) {
+//            if (product.getId() == idProduct) {
+//                return product.getName();
+//            }
+//        }
+//
+//        return null;
+//    }
+    //dùng stream
+    public static String filterProductById(List<Product> listProduct, int idProduct) {
+
+        return listProduct.stream()
+                .filter(product -> product.getId() == idProduct)
+                .map(Product::getName)
+                .findFirst()
+                .orElse(null);
+    }
+
+    public static void main(String[] args) {
+        List<Product> products = createListProduct();
+
+        String name = filterProductById(products,7);
+
+        System.out.println(name);
     }
 }
--- src/create_new_product/Product.java
+++ src/create_new_product/Product.java
@@ -1,14 +1,88 @@
-package createnewproduct;
+package create_new_product;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 public class Product {
 
     private int id;
     private String name;
     private int categoryId;
-    private Date saleDate;
+    private LocalDate saleDate;
     private int quantity;
     private boolean isDelete;
 
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", categoryId=" + categoryId +
+                ", saleDate=" + saleDate +
+                ", quantity=" + quantity +
+                ", isDelete=" + isDelete +
+                '}';
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public void setSaleDate(LocalDate saleDate) {
+        this.saleDate = saleDate;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setDelete(boolean delete) {
+        isDelete = delete;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public LocalDate getSaleDate() {
+        return saleDate;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public boolean isDelete() {
+        return isDelete;
+    }
+
+    public Product(int id,
+                   String name,
+                   int categoryId,
+                   LocalDate saleDate,
+                   int quantity,
+                   boolean isDelete) {
+
+        this.id = id;
+        this.name = name;
+        this.categoryId = categoryId;
+        this.saleDate = saleDate;
+        this.quantity = quantity;
+        this.isDelete = isDelete;
+    }
 }
